2a05ff2002aa5dff04aa4298bd899c024b300651
shelmith miano <mianoshelmith66@gmail.com>
2025-03-24 23:56:04 +0300

    useState

--- src/App.jsx
+++ src/App.jsx
@@ -19,34 +19,18 @@ export default function BlogPosts() {
  
   return (
     <div>
-      <Card style={{ width: '18rem' }}>
-      <Card.Img variant="top" src="holder.js/100px180" />
-       <Card.Body>
-        <Card.Title>Card Title</Card.Title>
-        <Card.Text>
-          Some quick example text to build on the card title and make up the
-          bulk of the card's content.
-        </Card.Text>
-        <Button variant="primary">Go somewhere</Button>
-      </Card.Body>
-    </Card>
+      {posts.map((post) => (
+         <Card style={{ width: '18rem' }}>
+         <Card.Img variant="top" src="https://picsum.photos/id/237/200/300" />
+          <Card.Body>
+           <Card.Title>{post.title}</Card.Title>
+           <Card.Text> {post.body}</Card.Text>
+           <Button variant="primary">View</Button>
+         </Card.Body>
+       </Card>
+      ))}
+     
     </div>
   );
-
-  return (
-    <Container>
-      {posts.map((post) => (
-        <Card style={{ width: "18rem" }} key={post.id}>
-          <Card.Img variant="top" src="https://via.placeholder.com/150/92c952" />
-          <Card.Body>
-            <Card.Title>{post.title}</Card.Title>
-            <Card.Text>{post.body.substring(0, 60)}...</Card.Text>
-            <Button variant="primary">View Post</Button>
-          </Card.Body>
-        </Card>
-      ))}
-    </Container>
-  );
-  
 }
 
